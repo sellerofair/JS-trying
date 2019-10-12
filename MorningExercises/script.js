0af6fc7d421e2fbf410c5a99af5f4ea35261c4ee
Akopyan Timur <56245133+sellerofair@users.noreply.github.com>
2019-10-12 16:44:45 +0300

    not alive

--- MorningExercises/script.js
+++ MorningExercises/script.js
@@ -1,1 +1,34 @@
-"use  strict"
+"use  strict";
+
+let index = 0;
+let shedule = [];
+
+function calculate() {
+    let beginning = new Date(document.getElementById("beginning").value);
+    index = (Date.now() - beginning) / (3600 * 24 * 1000);
+    index = 20 + Math.floor(index);
+    document.body.innerHTML = 
+`<table>
+    <tr>
+        <td>Press-ups</td>
+        <td>${index}</td>
+    </tr>
+    <tr>
+        <td colspan="2">Rest ${index} sec</td>
+    </tr>
+    <tr>
+        <td>ABS crunches</td>
+        <td>${index}</td>
+    </tr>
+    <tr>
+        <td colspan="2">Rest ${index} sec</td>
+    </tr>
+    <tr>
+        <td>Squats</td>
+        <td>${index}</td>
+    </tr>
+    <tr>
+        <td colspan="2">Rest ${index * 2} sec</td>
+    </tr>
+</table>`
+}
